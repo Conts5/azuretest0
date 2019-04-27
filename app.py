0fabaeb91da9b547ec0a6a34ac52a6b6aa014f82
jeff <adempus@pop-os.localdomain>
2019-04-26 22:46:22 -0400

    third commit

--- app.py
+++ app.py
@@ -5,16 +5,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello Azure Platform!'
+    return render_template('index.html')
 
 
 @app.route('/endpoint0')
 def endpoint_zero():
     return render_template('endpoint0.html')
 
+
 @app.route('/endpoint1')
 def endpoint_one():
     return render_template('endpoint1.html')
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0")
